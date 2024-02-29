7cd9e6c1659e5a3a2604ef8233cb4d40d9cbcf12
KwonSanta <KwonSanta@gmail.com>
2024-02-29 12:42:38 +0900

    BOJ p2754

---
+++ src/BOJ/p2754.java
@@ -0,0 +1,23 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class p2754 {
+    public static void main(String[] args) throws IOException {
+        String[] gradeTable = {"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D+", "D0", "D-", "F"};
+        double[] scoreTable = {4.3, 4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.7, 0};
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String grade = bf.readLine();
+
+        for (int i = 0; i < gradeTable.length; i++) {
+            if (gradeTable[i].equals(grade)) {
+                System.out.println(scoreTable[i]);
+            }
+        }
+
+    }
+}
