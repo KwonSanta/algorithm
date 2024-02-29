000e3557ed7440ca2bfa3d743433ccf3684e75bb
KwonSanta <KwonSanta@gmail.com>
2024-02-29 15:08:05 +0900

    BOJ p11718

---
+++ src/programmers/stringReverse.java
@@ -0,0 +1,15 @@
+package programmers;
+
+import java.util.Scanner;
+
+
+//Progra21Sremm3
+//Stanley1yelnatS
+public class stringReverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String my_string = sc.nextLine();
+
+        
+    }
+}
