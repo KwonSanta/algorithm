64459775aa553e65e3539a204ab7c2ccceeb8d1c
KwonSanta <KwonSanta@gmail.com>
2024-03-04 08:38:56 +0900

    p2475

---
+++ src/BOJ/p2475.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p2475 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int n3 = Integer.parseInt(st.nextToken());
+        int n4 = Integer.parseInt(st.nextToken());
+        int n5 = Integer.parseInt(st.nextToken());
+
+        int rst = ((n1*n1) + (n2*n2) + (n3*n3) + (n4*n4) + (n5*n5)) % 10;
+        System.out.println("rst = " + rst);
+    }
+}
